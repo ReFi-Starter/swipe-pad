256b23d672bf1861a86eca26519d0f08577077fb
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-05-04 08:19:40 +0200

    ✨ feat(api): add donation router with CRUD operations for donations

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc';
 import { userRouter } from './user';
 import { projectRouter } from './project';
+import { donationRouter } from './donation';
 
 export const appRouter = router({
   user: userRouter,
   project: projectRouter,
+  donation: donationRouter,
 });
 
 export type AppRouter = typeof appRouter; 
---
+++ src/server/routers/donation.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { publicProcedure, router } from '../trpc';
+import { donationRepository } from '@/repositories/donationRepository';
+
+export const donationRouter = router({
+  byProject: publicProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
+      return await donationRepository.getDonationsByProject(input);
+    }),
+    
+  byDonor: publicProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
+      return await donationRepository.getDonationsByDonor(input);
+    }),
+    
+  create: publicProcedure
+    .input(z.object({
+      txHash: z.string(),
+      donorAddress: z.string(),
+      projectId: z.string(),
+      amount: z.string(),
+      tokenAddress: z.string().optional(),
+    }))
+    .mutation(async ({ input }) => {
+      return await donationRepository.createDonation(input);
+    }),
+    
+  byTxHash: publicProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
+      return await donationRepository.getDonationByTxHash(input);
+    }),
+}); 
