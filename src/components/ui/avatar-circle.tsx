37d4d9a28653ec524858bbe4c1e9b1cc4ac92b58
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-05-05 03:50:46 +0200

    🎨 feat(ui): enhance AvatarCircle component with Next.js Image optimization

--- src/components/ui/avatar-circle.tsx
+++ src/components/ui/avatar-circle.tsx
@@ -2,6 +2,7 @@
 
 import { blo } from 'blo'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 interface AvatarCircleProps {
   className?: string
@@ -47,7 +48,15 @@ export function AvatarCircle({ className, address, onClick, isMiniPay }: AvatarC
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d="M13.3334 7.33333V8.66666H2.66669V7.33333H13.3334ZM8.66669 2.66666V13.3333H7.33335V2.66666H8.66669Z"
+              d="M14 4H12V3C12 2.45 11.55 2 11 2H2C1.45 2 1 2.45 1 3V11C1 11.55 1.45 12 2 12H4V13C4 13.55 4.45 14 5 14H14C14.55 14 15 13.55 15 13V5C15 4.45 14.55 4 14 4ZM2 11V3H11V11H2ZM14 13H5V12H11C11.55 12 12 11.55 12 11V4H14V13Z"
+              fill="#2563EB"
+            />
+            <path
+              d="M6.5 4.75C5.675 4.75 5 5.425 5 6.25C5 7.075 5.675 7.75 6.5 7.75C7.325 7.75 8 7.075 8 6.25C8 5.425 7.325 4.75 6.5 4.75Z"
+              fill="#2563EB"
+            />
+            <path
+              d="M3.5 9.5H9.5V8.5C9.5 7.675 7.825 7.25 6.5 7.25C5.175 7.25 3.5 7.675 3.5 8.5V9.5Z"
               fill="#2563EB"
             />
           </svg>
@@ -57,10 +66,12 @@ export function AvatarCircle({ className, address, onClick, isMiniPay }: AvatarC
   }
 
   return (
-    <img
+    <Image
       src={blo(address)}
       alt={`Address ${address}`}
       onClick={onClick}
+      width={32}
+      height={32}
       className={cn(
         'h-8 w-8 rounded-full border border-[#E0E0E0] cursor-pointer',
         className
